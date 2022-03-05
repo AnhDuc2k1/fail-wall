e55ca2c537013aa4855db6b577347bd76649c805
AnhDuc2k1 <anhduc2k1vn@gmail.com>
2022-03-05 16:25:04 +0700

    change the parameter of the method

--- src/main/java/org/aibles/failwall/user/services/IPasswordResetService.java
+++ src/main/java/org/aibles/failwall/user/services/IPasswordResetService.java
@@ -2,10 +2,8 @@ package org.aibles.failwall.user.services;
 
 import org.aibles.failwall.user.dtos.request.PasswordResetRequestDTO;
 
-import javax.servlet.http.HttpServletRequest;
-
 public interface IPasswordResetService {
 
-    void execute(PasswordResetRequestDTO passwordResetRequestDTO, HttpServletRequest request);
+    void execute(PasswordResetRequestDTO passwordResetRequestDTO);
 
 }
