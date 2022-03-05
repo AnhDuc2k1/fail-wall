af311cb2996fb7d58303cb1f26e65dc887ca727f
AnhDuc2k1 <anhduc2k1vn@gmail.com>
2022-03-05 15:40:53 +0700

    delete annotation @EnableAsync

--- src/main/java/org/aibles/failwall/mail/configuration/MailConfiguration.java
+++ src/main/java/org/aibles/failwall/mail/configuration/MailConfiguration.java
@@ -7,13 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.Properties;
 
 @Configuration
-@EnableAsync
 public class MailConfiguration {
 
     @Value("${spring.mail.username}")
