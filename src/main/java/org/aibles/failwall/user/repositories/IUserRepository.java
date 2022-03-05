227bdc2a4f8917e5f9be6a6ed44283ee8c0a7662
AnhDuc2k1 <anhduc2k1vn@gmail.com>
2022-03-05 15:48:02 +0700

    add query updatePassword

--- src/main/java/org/aibles/failwall/user/repositories/IUserRepository.java
+++ src/main/java/org/aibles/failwall/user/repositories/IUserRepository.java
@@ -1,6 +1,7 @@
 package org.aibles.failwall.user.repositories;
 
 import org.aibles.failwall.user.models.User;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +10,14 @@ import java.util.Optional;
 
 public interface IUserRepository extends CrudRepository<User, Long> {
 
-    @Query("SELECT u.user_id, u.name, u.email, u.password, u.is_actived FROM users u WHERE u.name = :username")
+    @Query("SELECT u.id, u.name, u.email, u.password, u.is_activated FROM users u WHERE u.name = :username")
     Optional<User> findUserByUsername(@Param("username") String username);
 
-    @Query("SELECT u.user_id, u.name, u.email, u.password, u.is_actived FROM users u WHERE u.email = :email")
+    @Query("SELECT u.id, u.name, u.email, u.password, u.is_activated FROM users u WHERE u.email = :email")
     Optional<User> findUserByEmail(@Param("email") String email);
+
+    @Modifying
+    @Query("UPDATE users SET password = :newPassword WHERE email = :email")
+    void updatePasswordForEmail(@Param("newPassword") String newPassword, @Param("email") String email);
 
 }
