42537f7e30f4e1e5f6d030d72e389b8a2ce7b73b
AnhDuc2k1 <anhduc2k1vn@gmail.com>
2022-03-05 15:42:57 +0700

    add class AsyncConfiguration

---
+++ src/main/java/org/aibles/failwall/user/configuration/AsyncConfiguration.java
@@ -0,0 +1,19 @@
+package org.aibles.failwall.user.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync(proxyTargetClass = true)
+@Configuration
+public class AsyncConfiguration {
+
+    @Bean("asyncExecutor")
+    public Executor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
+
+}
