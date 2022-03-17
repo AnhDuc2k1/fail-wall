4199815a0df532c159dab3f6585c23977189341e
trinh van dat <trinhvandat90399@gmail.com>
2022-03-18 04:50:18 +0700

    Update: Refactor api validate otp for active user

--- src/main/java/org/aibles/failwall/user/service/ActiveUserService.java
+++
@@ -1,9 +0,0 @@
-package org.aibles.failwall.user.service;
-
-import org.aibles.failwall.user.dto.request.ActiveUserFormRequestDto;
-
-public interface ActiveUserService {
-
-     void execute(ActiveUserFormRequestDto activeUserFormRequestDto);
-
-}
