ba72e765bf7bd803a77a66603ef5f594ef47c3f1
trinh van dat <trinhvandat90399@gmail.com>
2022-03-18 05:11:59 +0700

    Update: Refactor api User get otp for active

--- src/main/java/org/aibles/failwall/user/controller/UserOtpActivePostController.java
+++ src/main/java/org/aibles/failwall/user/controller/UserOtpActivePostController.java
@@ -1,8 +1,8 @@
 package org.aibles.failwall.user.controller;
 
 
-import org.aibles.failwall.user.dto.request.ActiveUserFormRequestDto;
-import org.aibles.failwall.user.service.GetOTPActiveUserService;
+import org.aibles.failwall.user.dto.request.UserOtpActiveReqDto;
+import org.aibles.failwall.user.service.UserOtpActiveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/user/get-otp")
-public class GetOtpCodeController {
+@RequestMapping("/api/v1/register/active")
+public class UserOtpActivePostController {
 
-    private final GetOTPActiveUserService service;
+    private final UserOtpActiveService service;
 
     @Autowired
-    public GetOtpCodeController(GetOTPActiveUserService service) {
+    public UserOtpActivePostController(UserOtpActiveService service) {
         this.service = service;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void execute(ActiveUserFormRequestDto activeUserFormRequestDto) {
-        service.execute(activeUserFormRequestDto);
+    public void execute(UserOtpActiveReqDto userOtpActiveReq) {
+        service.execute(userOtpActiveReq);
     }
 }
--- src/main/java/org/aibles/failwall/user/dto/request/UserRegisterActiveReqDto.java
+++ src/main/java/org/aibles/failwall/user/dto/request/UserRegisterActiveReqDto.java
@@ -1,18 +1,15 @@
 package org.aibles.failwall.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.aibles.failwall.validation.opt.OtpConstraint;
-import javax.validation.constraints.Email;
+
 import javax.validation.constraints.NotBlank;
 
-@Builder
 @Data
-public class UserRegisterActiveReqDto {
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
-    private String email;
+@Builder
+public class UserRegisterActiveReqDto extends UserOtpActiveReqDto {
 
     @OtpConstraint
     @NotBlank(message = "Otp is required")
