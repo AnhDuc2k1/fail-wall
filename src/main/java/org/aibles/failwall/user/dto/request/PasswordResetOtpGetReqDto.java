8ac833e1cf171b7a452c9953ee89c9f541c64a9c
trinh van dat <trinhvandat90399@gmail.com>
2022-03-18 07:16:31 +0700

    Update: api get otp for reset password

--- src/main/java/org/aibles/failwall/user/dto/request/PasswordResetOtpGetReqDto.java
+++ src/main/java/org/aibles/failwall/user/dto/request/PasswordResetOtpGetReqDto.java
@@ -1,19 +1,14 @@
 package org.aibles.failwall.user.dto.request;
 
+import lombok.Data;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-public class GetOTPResetPasswordRequestDTO {
+@Data
+public class PasswordResetOtpGetReqDto {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/org/aibles/failwall/user/service/IGetOTPResetPasswordService.java
+++
@@ -1,9 +0,0 @@
-package org.aibles.failwall.user.service;
-
-import org.aibles.failwall.user.dto.request.GetOTPResetPasswordRequestDTO;
-
-public interface IGetOTPResetPasswordService {
-
-    void execute(GetOTPResetPasswordRequestDTO forgotPasswordDTO);
-
-}
