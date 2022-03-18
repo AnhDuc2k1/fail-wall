b4cea70ba32c6387f5b932c9e40deed3f117581d
trinh van dat <trinhvandat90399@gmail.com>
2022-03-18 21:44:58 +0700

    Update UserRoleId.java

--- src/main/java/org/aibles/failwall/user/model/compositekey/UserRoleId.java
+++ src/main/java/org/aibles/failwall/user/model/compositekey/UserRoleId.java
@@ -1,31 +1,13 @@
 package org.aibles.failwall.user.model.compositekey;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRoleId {
-
-    private int userId;
-    private int roleId;
-
-    public UserRoleId() {
-    }
-
-    public UserRoleId(int userId, int roleId) {
-        this.userId = userId;
-        this.roleId = roleId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(int roleId) {
-        this.roleId = roleId;
-    }
+    private Long userId;
+    private Long roleId;
 }
