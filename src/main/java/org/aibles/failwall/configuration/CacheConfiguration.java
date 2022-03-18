34efb57095f23acef421983ecac2eff5cfbb3d78
trinh van dat <trinhvandat90399@gmail.com>
2022-03-18 08:43:48 +0700

    Update: Refactor api reset password for update new password

--- src/main/java/org/aibles/failwall/configuration/CacheConfiguration.java
+++ src/main/java/org/aibles/failwall/configuration/CacheConfiguration.java
@@ -12,11 +12,24 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfiguration {
 
     private static final int OTP_LIFE_TIME = 3;
+    private static final int PASSWORD_RESET_LIFE_TIME = 5;
 
-    @Bean
+    @Bean("otpCache")
     public LoadingCache<String, String> createOtpCacheBean() {
         return CacheBuilder.newBuilder()
                 .expireAfterWrite(OTP_LIFE_TIME, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) throws Exception {
+                        return key.toUpperCase();
+                    }
+                });
+    }
+
+    @Bean("passwordResetCache")
+    public LoadingCache<String, String> createPasswordResetCacheBean() {
+        return CacheBuilder.newBuilder()
+                .expireAfterWrite(PASSWORD_RESET_LIFE_TIME, TimeUnit.MINUTES)
                 .build(new CacheLoader<String, String>() {
                     @Override
                     public String load(String key) throws Exception {
--- src/main/java/org/aibles/failwall/user/service/IPasswordResetService.java
+++
@@ -1,9 +0,0 @@
-package org.aibles.failwall.user.service;
-
-import org.aibles.failwall.user.dto.request.PasswordResetRequestDTO;
-
-public interface IPasswordResetService {
-
-    void execute(PasswordResetRequestDTO passwordResetRequestDTO);
-
-}
